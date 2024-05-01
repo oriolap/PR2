b054c53094a1bb76463c77b30bf950ea3531bebf
oriolap <155771803+oriolap@users.noreply.github.com>
2024-05-01 22:44:03 +0200

    Update weighing.c

--- weighing.c
+++ weighing.c
@@ -79,47 +79,53 @@ bool weighingList_empty(tWeighingList list)
 tApiError weighingList_add(tWeighingList* list, tWeighing weighing)
 {
 	// PR2 EX 1c
-	tWeighingNode* node;
-    node = weighingList_createNode(weighing);
     
+    //create node
+	tWeighingNode* node = weighingList_createNode(weighing);
+    
+    //check if node is properly created
     if (node == NULL){
         return E_MEMORY_ERROR;
     }
     
+    //check if node with the same date and code already exists. In case it does add the new node weight to it
+    tWeighingNode* existingNode;
+    existingNode = weighingList_findNode(*list, node->elem.code, node->elem.harvestDay);
+    
+    if(existingNode !=NULL){
+        existingNode->elem.weight += node->elem.weight;
+        return E_SUCCESS;
+    }
+    
+    //add node as the first and last element if the list is empty
     if (list->first == NULL && list->last == NULL){
         list->first = node;
         list->last = node; 
         return E_SUCCESS;
     }
     
+    //find the previous node where insertion of the new one must be done
     tWeighingNode* previousNode;
     previousNode = weighingList_findPrev(*list, node->elem.code, node->elem.harvestDay);
     
+    //if pervious node is NULL, it means the node should be inserted in the first position of the list
     if (previousNode == NULL){
         node->next = list->first;
         list->first->prev = node;
         list->first = node;
     } else {
-        // Insert in the middle or at the end
+        //insert node in the middle of the list
         node->next = previousNode->next;
         if (previousNode->next != NULL) {
             previousNode->next->prev = node;
         } else {
-            // New node becomes the last node
+            //insert node at the end of the list
             list->last = node;
         }
         previousNode->next = node;
         node->prev = previousNode;
     }
     
-    /*if (previousNode != NULL){
-        node->prev = previousNode;
-        node->next = previousNode->next;
-        previousNode->next->prev = node;
-        previousNode->next = node;
-        
-    }*/
-    /////////////////////////////////
     return E_SUCCESS;
 }
 
@@ -127,9 +133,28 @@ tApiError weighingList_add(tWeighingList* list, tWeighing weighing)
 double weighingList_getWeight(tWeighingList list, const char* code, tDate day)
 {
 	// PR2 EX 1d
-	
+	double totalWeight;
+    totalWeight = 0.0;
+    
+    tWeighingNode* currentNode;
+    currentNode = list.first;
+    
+    while(currentNode != NULL){
+        
+        if (date_cmp(currentNode->elem.harvestDay, day) == -1){
+            totalWeight = totalWeight + currentNode->elem.weight;
+        }
+        
+        if (strcmp(currentNode->elem.code, code) == 0){
+            totalWeight = totalWeight + currentNode->elem.weight;
+        }
+        
+        
+        currentNode = currentNode->next;
+    }
+
     /////////////////////////////////
-    return 0.0;
+    return totalWeight;
 }
 
 // Release the list
@@ -160,30 +185,40 @@ tWeighingNode* weighingList_createNode(tWeighing weighing)
 // Find a node by harvest date and code
 tWeighingNode* weighingList_findNode(tWeighingList list, const char* code, tDate harvestDay){
     
+    tWeighingNode* currentNode = list.first;
+    
+    while(currentNode != NULL){
+        if ((date_cmp(currentNode->elem.harvestDay, harvestDay) == 0) && (strcmp(currentNode->elem.code, code) == 0)){
+            return currentNode; 
+        }
+           
+        currentNode = currentNode->next;
+    }
+    
+    return NULL;
+    
 }
 
 // Find the previous node to insert a new node by its harvest day and its code
 tWeighingNode* weighingList_findPrev(tWeighingList list, const char* code, tDate harvestDay){
-    // set as a currentNode the first element of the list
-    tWeighingNode* currentNode = list.first;
     
-    //iterate through the list while the current node is not null, and it's not the last element of the list. 
-    while(currentNode != NULL && currentNode->next != NULL){
-        if (date_cmp(currentNode->elem.harvestDay, harvestDay) == -1 && (date_cmp(currentNode->next->elem.harvestDay, harvestDay) == 1)){
+    // set as a currentNode the last element of the list
+
+    tWeighingNode* currentNode = list.last;
+    
+    //if the weighing to add has a newest date than the current node, return the current node 
+    while(currentNode!= NULL){
+        if(date_cmp(harvestDay, currentNode->elem.harvestDay) == 1){
             return currentNode;
         }
-        if (date_cmp(currentNode->elem.harvestDay, harvestDay) == 0 && (strcmp(currentNode->elem.code, code) > 0)){
-            return currentNode; 
+        //if the weighing to add has the same date but smaller code, return the current node
+        if((date_cmp(harvestDay, currentNode->elem.harvestDay) == 0) && (strcmp(code, currentNode->elem.code) > 0)){
+            return currentNode;
         }
-        currentNode = currentNode->next;
-    }
-
-    //in case the new node has the newest date, return the last node of the list.      
-    if(date_cmp(list.last->elem.harvestDay, harvestDay) == -1){
-        currentNode = list.last;
-        return currentNode; 
+        currentNode = currentNode->prev;
     }
     
+    //if none of the nodes in the list meets the previous requirements return NULL. 
     return NULL;
-   
+
 }
